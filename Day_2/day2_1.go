50548e1ab7a0239156575ec6e0f04245769f9d5e
yuriyskrypnyk <skrypnyk81@gmail.com>
2022-12-04 19:37:23 +0100

    Second day

---
+++ Day_2/day2_1.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	readFile, err := os.Open("scissors.txt")
+
+	if err != nil {
+		fmt.Println("Errore in lettura del file", err)
+	}
+	scanner := bufio.NewScanner(readFile)
+	total := 0
+	myScore := map[string]int{
+		"Y": 2,
+		"Z": 3,
+		"X": 1,
+	}
+	myPair := map[string]int{
+		"A X": 3,
+		"A Y": 6,
+		"B Y": 3,
+		"B Z": 6,
+		"C Z": 3,
+		"C X": 6,
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := strings.Fields(line)
+		total += myScore[words[1]]
+		listItem := strings.Join(words, " ")
+		total += myPair[listItem]
+	}
+
+	fmt.Println(total)
+	readFile.Close()
+}
+
+// A, X = Rock
+// B, Y = paper
+// C, Z = Scissors
---
+++ Day_2/day2_2.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	readFile, err := os.Open("scissors.txt")
+
+	if err != nil {
+		fmt.Println("Errore in lettura del file", err)
+	}
+	scanner := bufio.NewScanner(readFile)
+	total := 0
+	myScore := map[string]int{
+		"B": 2,
+		"C": 3,
+		"A": 1,
+	}
+	myScoreNormal := map[string]int{
+		"Y": 2,
+		"Z": 3,
+		"X": 1,
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := strings.Fields(line)
+		if words[1] == "Y" {
+			total += myScore[words[0]]
+			total += 3
+		} else if words[1] == "X" {
+			if words[0] == "A" {
+				total += myScoreNormal["Z"]
+			} else if words[0] == "B" {
+				total += myScoreNormal["X"]
+			} else if words[0] == "C" {
+				total += myScoreNormal["Y"]
+			}
+
+		} else if words[1] == "Z" {
+			if words[0] == "A" {
+				total += myScoreNormal["Y"]
+			} else if words[0] == "B" {
+				total += myScoreNormal["Z"]
+			} else if words[0] == "C" {
+				total += myScoreNormal["X"]
+			}
+			total += 6
+		}
+
+	}
+
+	fmt.Println(total)
+	readFile.Close()
+}
