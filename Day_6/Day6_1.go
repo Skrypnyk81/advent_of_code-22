ff1c1bf8b1884ef288af8410612f3ff465fe1bab
yuriyskrypnyk <skrypnyk81@gmail.com>
2022-12-22 22:00:37 +0100

    Sixth day

---
+++ Day_6/Day6_1.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func unique(arr string) bool {
+	m := make(map[rune]bool)
+	for _, i := range arr {
+		_, ok := m[i]
+		if ok {
+			return false
+		}
+
+		m[i] = true
+	}
+
+	return true
+}
+
+func main() {
+
+	readFile, err := os.Open("signals.txt")
+
+	if err != nil {
+		fmt.Println("Errore in lettura del file", err)
+	}
+	scanner := bufio.NewScanner(readFile)
+
+	for scanner.Scan() {
+		lineSector := scanner.Text()
+		for i := 0; i < len(lineSector)-4; i++ {
+			subString := lineSector[i : i+4]
+			check := unique(subString)
+			if check == true {
+				fmt.Println(i + 4)
+				break
+			}
+		}
+		err = readFile.Close()
+		if err != nil {
+			fmt.Println("Errore nella chiusura del file", err)
+		}
+	}
+}
